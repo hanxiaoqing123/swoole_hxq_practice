f76e991b89dac9051a98ef464c17f5bc115dc2c7
hanxiaoqing123 <1695722217@qq.com>
2017-05-23 10:00:26 +0800

    swoole定时器

--- timer-ticker/tick-server.php
+++ timer-ticker/tick-server.php
@@ -4,6 +4,7 @@ $serv->set([
     'worker_num'=>2,
 ]);
 $serv->on('WorkerStart',function ($serv,$workerId){
+    //注意此处开启了两个Worker进程，如果不判断，就会在两个Worker进程内各注册一个定时器，实际上也就是注册了两个相同的定时器，这是没必要的。
     if($workerId==0){
         $i=0;
         $params='world';
---
+++ timer-ticker/timer-client.php
@@ -0,0 +1,5 @@
+<?php
+$client=new swoole_client(SWOOLE_SOCK_TCP,SWOOLE_SOCK_SYNC);
+$client->connect('127.0.0.1',9501) || exit("connect failed .Error:{$client->errCode}\n");
+$client->send("hello server.");
+$client->close();
