5c5275663973e3fa2bbb86caad95798bf85242d9
hanxiaoqing123 <1695722217@qq.com>
2017-05-23 09:59:37 +0800

    弃用文件

--- timer-ticker/client-first.php
+++
@@ -1,7 +0,0 @@
-<?php
-$client=new swoole_client(SWOOLE_SOCK_TCP,SWOOLE_SOCK_SYNC);
-$client->connect('127.0.0.1',9501) || exit("connect failed .Error:{$client->errCode}\n");
-$client->send("hello server.");
-$response=$client->recv();
-echo $response.PHP_EOL;
-$client->close();
